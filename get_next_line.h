f600c62def0850e2b6932d8592b187187c95a2b4
juvilleg <jvillegasmartin04@gmail.com>
2023-11-24 12:02:57 +0100

    Prototipos

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juvilleg <juvilleg@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 11:58:37 by juvilleg          #+#    #+#             */
+/*   Updated: 2023/11/24 12:01:38 by juvilleg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LIBE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
