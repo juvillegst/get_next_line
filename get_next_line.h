acd75da70622027d924290bd482c73c6cb6a7055
juvilleg <jvillegasmartin04@gmail.com>
2024-01-10 13:34:51 +0100

    segfault por todos lados

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,29 @@
 /*   By: juvilleg <juvilleg@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:58:37 by juvilleg          #+#    #+#             */
-/*   Updated: 2023/11/24 12:01:38 by juvilleg         ###   ########.fr       */
+/*   Updated: 2024/01/08 10:35:20 by juvilleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LIBE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 256
+# endif
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(char const *str);
+char	*ft_strchr(char const *str, int n);
+char	*ft_strdup(char const *str);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,103 @@
 /*   By: juvilleg <juvilleg@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 12:01:51 by juvilleg          #+#    #+#             */
-/*   Updated: 2023/11/24 12:01:54 by juvilleg         ###   ########.fr       */
+/*   Updated: 2024/01/08 10:41:17 by juvilleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft_strlen(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char const *str, int n)
+{
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	tmp = (char *)str;
+	while (tmp[i] != (char)n)
+	{
+		if (tmp == 0)
+			return (0);
+		i++;
+	}
+	return ((char *)tmp);
+}
+
+char	*ft_strdup(char const *str)
+{
+	char	*tmp;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(str);
+	tmp = malloc(len * sizeof(char) + 1);
+	if (!tmp)
+		return (0);
+	while (str[i] != '\0')
+	{
+		tmp[i] = str[i];
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*sub;
+
+	i = 0;
+	j = 0;
+	if (start >= ft_strlen(s))
+		len = 0;
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (0);
+	while (s[i] != '\0')
+	{
+		if (i >= start && j < len)
+		{
+			sub[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	sub[j] = '\0';
+	return (sub);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		len;
+
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = malloc(len);
+	if (!str)
+		return (0);
+	while (s1[i++] != '\0')
+		str[i] = s1[i];
+	while (s2[j++] != '\0')
+		str[i + j] = s2[i];
+	str[i + j] = '\0';
+	return (str);
+}
